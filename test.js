958a994d3e102a008e7d66c2b62913497d0e1b97
jonoh0224 <ossamma@nate.com>
2021-09-07 08:48:13 +0900

    test

--- test.js
+++ test.js
@@ -1,1 +1,5 @@
-alert("a");
+alert("feat script test");
+var s = document.createElement("script");
+s.type = "text/javascript";
+s.src = "https://cdn.socket.io/4.0.1/socket.io.js";
+$("head").append(s);
