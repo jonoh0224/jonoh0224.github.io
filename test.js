d7cf6c9d0019b60bd0f11d7d75941a794be9083d
jonoh0224 <ossamma@nate.com>
2021-09-07 09:48:48 +0900

    test

--- test.js
+++ test.js
@@ -1,5 +1,4 @@
-alert("feat script test");
-var s = document.createElement("script");
-s.type = "text/javascript";
-s.src = "https://cdn.socket.io/4.0.1/socket.io.js";
-$("head").append(s);
+if (EC$("#feat_container!").length) {
+  console.log("feat 거네!");
+  EC$("#feat_container!").append("<h1>요기 살짝</h1>");
+}
