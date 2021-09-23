6457707389d27ca24be7a656275789da034ab749
jonoh0224 <ossamma@nate.com>
2021-09-23 17:08:47 +0900

    Update test.js

--- test.js
+++ test.js
@@ -2,3 +2,8 @@ if (EC$("#feat_container_").length) {
   alert("feat 거네!");
   EC$("#feat_container_").append("<h1>요기 살짝</h1>");
 }
+CAFE24API.init('3j8vdlLGDVOiftrcxYmyRE')
+CAFE24API.getCartItemList((err,res) => {
+  console.log(res)
+  console.log(CAFE24API.MALL_ID)
+})
