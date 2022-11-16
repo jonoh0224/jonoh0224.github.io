575c781c9c0d0e662f48c112fc68e0b78f89b07c
Jonghyuk Oh <ossamma@nate.com>
2022-11-16 15:03:35 +0900

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-if (EC$("#feat_container_").length) {
-  alert("feat 거네!");
-  EC$("#feat_container_").append("<h1>요기 살짝</h1>");
-}
-CAFE24API.init('3j8vdlLGDVOiftrcxYmyRE')
-CAFE24API.getCartItemList((err,res) => {
-  console.log(res)
-  console.log(CAFE24API.MALL_ID)
-})
